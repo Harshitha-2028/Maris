3c9b4bd8e6c02ee95991eea25df77d9106573df5
alek-7 <alekyavarmaakhi7@gmail.com>
2025-09-23 05:07:25 +0530

    Update babel.config.js

--- sih frontend/babel.config.js
+++ sih frontend/babel.config.js
@@ -1,34 +1,20 @@
-
 module.exports = function (api) {
-
-api.cache(true);
-
-return {
-
-presets: ["babel-preset-expo"],
-
-plugins: [
-
-"nativewind/babel",
-
-["module-resolver", {
-
-alias: {
-
-"@components": "./components",
-
-"@screens": "./screens",
-
-"@assets": "./assets",
-
-},
-
-}],
-
-"react-native-reanimated/plugin",
-
-],
-
-};
-
+  api.cache(true);
+  return {
+    presets: ["babel-preset-expo"],
+    plugins: [
+      "nativewind/babel",
+      [
+        "module-resolver",
+        {
+          alias: {
+            "@components": "./components",
+            "@screens": "./screens",
+            "@assets": "./assets"
+          }
+        }
+      ],
+      "react-native-reanimated/plugin"
+    ]
+  };
 };
